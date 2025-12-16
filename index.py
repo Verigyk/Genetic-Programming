d89ee9b00808d96db0b3460e6f97f9dc035734e4
Verigyk <47785872+Verigyk@users.noreply.github.com>
2025-12-16 13:19:26 +0100

    stiiiiiiiiiiiiiil gooooooooooooood

--- index.py
+++ index.py
@@ -1441,9 +1441,9 @@ if __name__ == "__main__":
                 elitism_count=3,
                 random_injection_count=3,
                 fitness_threshold=0.95,
-                max_depth_range=(1, 3),
+                max_depth_range=(1, 8),
                 max_children_range=(1, 3),
-                feature_range=(5, 10),
+                feature_range=(5, 90),
                 use_real_fitness=SKSURV_AVAILABLE,  # Active si disponible
                 dataframes=loader.data_frames,
                 y = loader.y,
